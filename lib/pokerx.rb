b7dd9f4bd9540e67b65c3cd91f29f44257a676e3
Ruben Rhodes & Patrick David <student@epicodus.com>
2014-07-24 12:27:04 -0700

    YOu can get pairs, or a three of a kind

--- lib/pokerx.rb
+++ lib/pokerx.rb
@@ -21,30 +21,42 @@ def pokerx(hand)
     end
     i = i +1
   end
-  puts(value_card)
-
-  if value_card.count("5") == 2
-    winner = winninghand[1]
-  end
   if value_card[0].to_i + 1 == value_card[1].to_i && value_card[1].to_i + 1 == value_card[2].to_i && value_card[2].to_i + 1 == value_card[3].to_i && value_card[3].to_i + 1 == value_card[4].to_i
     winner = winninghand[4]
   end
-  winner
-  # puts(sorted_suits)
-  # puts(value_card)
+
+store = []
+
+count = 0
+value_card.each do |value_of_card|
+    if value_card[count] != value_card[count +1]
+    store.push(value_card[count])
+    count +=1
+  else
+    count +=1
+  end
 end
 
-# card_test = [3,3,4,5,5,5,"J","J","J","A"]
+count = 0
+card_counter = []
+store.each do |value_of_card|
+  card_counter.push(value_card.count(value_of_card))
+end
+puts(card_counter)
+
+if card_counter == [1,1,1,2] || card_counter == [1,1,2,1] || card_counter == [1,2,1,1] || card_counter == [2,1,1,1]
+  winner = winninghand[1]
+end
+
+if card_counter == [1,2,2] || card_counter == [2,2,1] || card_counter == [1,2,1] || card_counter == [2,1,2]
+  winner = winninghand[2]
+end
 
 
-# count = 0
-# card_test.each do |value_of_card|
-#   store = []
-#   if card_test[count] != card_test[count +1]
-#     store.push(card_test[count])
-#     count +=1
-#   else
-#     count +=1
-#   end
-#   puts(store)
-# end
+if card_counter == [1,1,3] || card_counter == [1,3,1] || card_counter == [3,1,1]
+  winner = winninghand[3]
+end
+
+
+winner
+end
--- spec/pokerx_spec.rb
+++ spec/pokerx_spec.rb
@@ -5,10 +5,13 @@ describe('pokerx') do
   it('if you have two cards of the same value, it will return pair') do
     expect(pokerx(['2C', '5S', '3H', 'JD', '5H'])).to(eq("pair"))
   end
+  it('if you have two cards of the same value, it will return pair') do
+    expect(pokerx(['2C', '2S', 'JH', 'JD', '5H'])).to(eq("two-pair"))
+  end
   it('if you have five cards and each card is 1 larger than the previous card, return straight') do
     expect(pokerx(['2C', '3S', '4H', '5D', '6H'])).to(eq("straight"))
   end
   it('if you have three cards of the same value, it will return three-of-a-kind') do
-    expect(pokerx(['2C', '3S', '4H', '5D', '6H'])).to(eq("straight"))
+    expect(pokerx(['JC', 'KS', 'KH', 'KD', '6H'])).to(eq("three-of-a-kind"))
   end
 end
