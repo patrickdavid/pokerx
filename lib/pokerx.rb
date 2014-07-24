2697d6b79409a4f81b52886c537ad86745f7670e
Ruben Rhodes & Patrick David <student@epicodus.com>
2014-07-24 15:35:56 -0700

    YOu have the ability to hands like full house, 4 of a kind, straight- flushes and royal flush

--- lib/pokerx.rb
+++ lib/pokerx.rb
@@ -1,29 +1,64 @@
 def pokerx(hand)
   suits = []
-  value_card =[]
+  og_value_card =[]
   winninghand = ["high-card", "pair", "two-pair", "three-of-a-kind", "straight", "flush", "full-house", "four-of-a-kind", "straight-flush", "royal-flush"]
   sortedhand = hand.sort
-  sortedhand.each{|card| value_card.push(card[0])}
+
+
+  sortedhand.each{|card| og_value_card.push(card[0])}
   sortedhand.each{|card| suits.push(card[1])}
   sorted_suits = suits.sort
 
-  i = 0
-  value_card.each do |value|
-    if value == "J"
-      value_card[i] = 11
-    elsif value == "Q"
-      value_card[i] = 12
-    elsif value == "K"
-      value_card[i] = 13
-    elsif value == "A"
-      value_card == 14
+  z = 0
+  sorted_suits.each do |suit|
+    if suit == "0"
+      sorted_suits[z] = sortedhand[z][2]
+      z =z+1
+    else
+      z =z+1
+    end
+  end
 
+
+  i = 0
+  og_value_card.each do |value|
+    if value == "1"
+      og_value_card[i] = 10
+    elsif value == "J"
+      og_value_card[i] = 11
+    elsif value == "Q"
+      og_value_card[i] = 12
+    elsif value == "K"
+      og_value_card[i] = 13
+    elsif value == "A"
+      og_value_card[i] = 14
+    elsif value == "a"
+      og_value_card[i] = 1
+    else
+      og_value_card[i] = og_value_card[i].to_i
     end
     i = i +1
   end
-  if value_card[0].to_i + 1 == value_card[1].to_i && value_card[1].to_i + 1 == value_card[2].to_i && value_card[2].to_i + 1 == value_card[3].to_i && value_card[3].to_i + 1 == value_card[4].to_i
-    winner = winninghand[4]
+
+value_card = og_value_card.sort
+
+  if (value_card.inject(0) {|sum, i| sum +i} == 60) && sorted_suits[0] == sorted_suits[1] && sorted_suits[1] == sorted_suits[2] && sorted_suits[2] == sorted_suits[3] && sorted_suits[3] == sorted_suits[4]
+    winner = winninghand[9]
+  elsif sorted_suits[0] == sorted_suits[1] && sorted_suits[1] == sorted_suits[2] && sorted_suits[2] == sorted_suits[3] && sorted_suits[3] == sorted_suits[4] && value_card[0] + 1 == value_card[1] && value_card[1] + 1 == value_card[2] && value_card[2] + 1 == value_card[3] && value_card[3] + 1 == value_card[4]
+    winner = winninghand[8]  # straight flush
+  elsif
+    value_card[0] + 1 == value_card[1] && value_card[1] + 1 == value_card[2] && value_card[2] + 1 == value_card[3] && value_card[3] + 1 == value_card[4]
+    winner = winninghand[4]  #straight
+  elsif
+    sorted_suits[0] == sorted_suits[1] && sorted_suits[1] == sorted_suits[2] && sorted_suits[2] == sorted_suits[3] && sorted_suits[3] == sorted_suits[4]
+    winner = winninghand[5]#flush
+  elsif value_card[0] != value_card[1] && value_card[1] != value_card[2] && value_card[2]!= value_card[3] && value_card[3] != value_card[4]
+    winner = winninghand[0]
+
+
+
   end
+
 
 store = []
 
@@ -42,20 +77,28 @@ card_counter = []
 store.each do |value_of_card|
   card_counter.push(value_card.count(value_of_card))
 end
-puts(card_counter)
+
 
 if card_counter == [1,1,1,2] || card_counter == [1,1,2,1] || card_counter == [1,2,1,1] || card_counter == [2,1,1,1]
-  winner = winninghand[1]
+  winner = winninghand[1] #pair
 end
 
 if card_counter == [1,2,2] || card_counter == [2,2,1] || card_counter == [1,2,1] || card_counter == [2,1,2]
-  winner = winninghand[2]
+  winner = winninghand[2] #two-pair
 end
-
 
 if card_counter == [1,1,3] || card_counter == [1,3,1] || card_counter == [3,1,1]
-  winner = winninghand[3]
+  winner = winninghand[3] #3-of-a-kind
 end
+
+if card_counter == [1,4] || card_counter == [4,1]
+  winner = winninghand[7] #4-of-a-kind
+end
+
+if card_counter == [2,3] || card_counter == [3,2]
+  winner = winninghand[6] #4-of-a-kind
+end
+
 
 
 winner
--- spec/pokerx_spec.rb
+++ spec/pokerx_spec.rb
@@ -2,16 +2,43 @@ require('rspec')
 require('pokerx')
 
 describe('pokerx') do
-  it('if you have two cards of the same value, it will return pair') do
+  it('if you have two cards of the same value, will return pair') do
     expect(pokerx(['2C', '5S', '3H', 'JD', '5H'])).to(eq("pair"))
   end
-  it('if you have two cards of the same value, it will return pair') do
+
+  it('if you have two cards of the same value, return pair') do
     expect(pokerx(['2C', '2S', 'JH', 'JD', '5H'])).to(eq("two-pair"))
   end
+
   it('if you have five cards and each card is 1 larger than the previous card, return straight') do
-    expect(pokerx(['2C', '3S', '4H', '5D', '6H'])).to(eq("straight"))
+    expect(pokerx(['10C', 'JS', 'QH', 'KD', 'AH'])).to(eq("straight"))
   end
-  it('if you have three cards of the same value, it will return three-of-a-kind') do
+
+  it('if you have three cards of the same value, return three-of-a-kind') do
     expect(pokerx(['JC', 'KS', 'KH', 'KD', '6H'])).to(eq("three-of-a-kind"))
+  end
+
+  it('if you have four cards of the same value, return four-of-a-kind') do
+    expect(pokerx(['5C', 'KS', '5H', '5D', '5S'])).to(eq("four-of-a-kind"))
+  end
+
+  it('if you have five cards of the same suit but different value, return flush') do
+    expect(pokerx(['5C', 'KC', '7C', '3C', 'JC'])).to(eq("flush"))
+  end
+
+  it('if you have five cards of the same suit in order that includes two number cards, return staight flush') do
+    expect(pokerx(['JS', '7S', '8S', '9S', '10S'])).to(eq("straight-flush"))
+  end
+
+  it('if you have five cards of the same suit in order that includes A,K,Q,J,10: return royal flush') do
+    expect(pokerx(['10S', 'JS', 'QS', 'KS', 'AS'])).to(eq("royal-flush"))
+  end
+
+  it('if you have five cards of differing values') do
+    expect(pokerx(['4S', 'JH', 'KS', '2S', 'AS'])).to(eq("high-card"))
+  end
+
+  it('if you have 3-of a kind and a pair') do
+    expect(pokerx(['4D', '4H', '4S', 'AS', 'AS'])).to(eq("full-house"))
   end
 end
